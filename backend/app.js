d96a1e1ef21f67a53b56dfe0c6d2c1a8e1b6fbd7
idealatom <idealatom@gmail.com>
2023-11-18 15:51:06 +0300

    work on queue processing

--- backend/app.js
+++ backend/app.js
@@ -32,8 +32,13 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  const err_code = err.status || 500;
+  res
+  .status(err_code)
+  .json({
+    error: err_code,
+  })
+  .end();
 });
 
 module.exports = app;
--- backend/queue.js
+++ backend/queue.js
@@ -1,17 +1,55 @@
 const Queue = require('bull');
+const { ethers } = require("ethers");
+
+const provider = new ethers.AlchemyProvider('sepolia', process.env.ALCHEMY_API_KEY);
+const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
 const proofsQueue = new Queue('proofs', 'redis://127.0.0.1:6379');
 
 proofsQueue.process(async (job, done) => {
-  const data = job.data
-  console.log(`Processing Job #${job.id} with data ${JSON.stringify(data)}...`);
-  const DELAY = 10000;
-  await new Promise(resolve => setTimeout(resolve, DELAY));
-  done(null, { result: 'result'});
+  try {
+    const data = job.data
+    console.log(`Processing Job #${job.id} with data ${JSON.stringify(data)}...`);
+
+    // const { chainId, proposalId, token, voter } = job.data.token;
+    // const proof = await generateProof();
+    // const result = await castVote();
+
+    const balance = (await provider.getBalance(data.voter)).toString();
+    const result = { balance: ethers.formatUnits(balance)}
+
+    done(null, result);
+  } catch (e) {
+    console.error(e);
+    done(new Error('Job processing error'));
+  }
 });
 
 proofsQueue.on('completed', (job, result) => {
   console.log(`Job #${job.id} completed with result ${JSON.stringify(result)}`);
 })
+
+async function generateProof(chainId, proposalId, token, voter) {
+  // TODO
+  const root = await getL2Root(proposalId, chainId);
+  const SLEEP_DELAY = 60 * 1000;
+  await new Promise(resolve => setTimeout(resolve, SLEEP_DELAY));
+}
+
+async function getL2Root(proposalId, chainId) {
+  // TODO: read from GovernorL1.sol state
+  return null
+}
+
+async function castVote() {
+  // TODO: call GovernorL1.castVoteCC
+  // function castVoteCC(
+  //   uint256 proposalId,
+  //   address voter,
+  //   uint8 support,
+  //   uint256 weight,
+  //   uint256 chainId,
+  //   bytes calldata proof) public returns (uint256)
+}
 
 module.exports = proofsQueue;
