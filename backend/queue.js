8da22d3e7a421e8cfd22690f3142a9c3fcb1f584
idealatom <idealatom@gmail.com>
2023-11-19 02:45:47 +0300

    read governor address from env

--- backend/queue.js
+++ backend/queue.js
@@ -50,8 +50,7 @@ async function generateProof(chainId, stateRoot, block, token, voter) {
 }
 
 async function castVote(chainId, proposalId, voter, support, weight, proof) {
-  const GOVERNOR_ADDRESS = '0x990cD37e51a60A7D0756CA671A80e4A33FB0413A'
-  const GovernorL1 = new ethers.Contract(GOVERNOR_ADDRESS, GovernorL1Meta.abi, signerL1);
+  const GovernorL1 = new ethers.Contract(process.env.GOVERNOR_L1, GovernorL1Meta.abi, signerL1);
   const tx = await GovernorL1.castVoteCC(proposalId, voter, support, weight, chainId, proof)
   await tx.wait();
   return tx
