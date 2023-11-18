83ba732fb41a1ce3ff5ec9939edae25012b4dbf1
Artem B <hoongun@gmail.com>
2023-11-19 01:36:39 +0300

    multichain supported

--- backend/rootWorkerLogs.js
+++ backend/rootWorkerLogs.js
@@ -16,16 +16,22 @@ const chainCfg = {
   1: {
     chainId: 1,
     providerUrl: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
+    tokenL2: '0xdAC17F958D2ee523a2206206994597C13D831ec7',  // USDT
+    fromBlock: 18597627,
   },
   50: {
-    chainId: 50,
     // XDC Network
+    chainId: 50,
     // providerUrl:
   },
   51: {
     // XDC Apothem Network
     chainId: 51,
     providerUrl: `https://erpc.apothem.network`,
+    // tokenL2: '0xDf29cB40Cb92a1b8E8337F542E3846E185DefF96',  // FXD
+    // fromBlock: 57005948,
+    tokenL2: '0xbb8c8E79c34C6420716A6937bF7E3B9226Cc81f5',
+    fromBlock: 57052616,
   },
   11155111: {
     // Sepolia
@@ -36,6 +42,50 @@ const chainCfg = {
     // Scroll Sepolia Testnet
     chainId: 534351,
     providerUrl: `https://scroll-testnet-public.unifra.io`,
+    tokenL2: '0xC007267DF5f0f7aEc5fb90CF03b56F051Bc6C89e',
+    fromBlock: 2303587,
+  },
+  421614: {
+    // Arbitrum Sepolia
+    chainId: 421614,
+    providerUrl: `https://sepolia-rollup.arbitrum.io/rpc`,
+    tokenL2: '0xC007267DF5f0f7aEc5fb90CF03b56F051Bc6C89e',
+    fromBlock: 1234215,
+  },
+  1442: {
+    // Polygon zkEVM Testnet
+    chainId: 1442,
+    providerUrl: `https://rpc.public.zkevm-test.net`,
+    tokenL2: '0xC007267DF5f0f7aEc5fb90CF03b56F051Bc6C89e',
+    fromBlock: 3222705,
+  },
+  100: {
+    // Gnosis
+    chainId: 100,
+    providerUrl: `https://gnosis.drpc.org`,
+    tokenL2: '0xbb8c8E79c34C6420716A6937bF7E3B9226Cc81f5',
+    fromBlock: 31014819,
+  },
+  44787: {
+    // Celo Alfajores Testnet
+    chainId: 44787,
+    providerUrl: `https://alfajores-forno.celo-testnet.org`,
+    tokenL2: '0xC007267DF5f0f7aEc5fb90CF03b56F051Bc6C89e',
+    fromBlock: 20989273,
+  },
+  84531: {
+    // Base Goerli Testnet
+    chainId: 84531,
+    providerUrl: `https://base-goerli.publicnode.com`,
+    tokenL2: '0x6c9373BaD4d213Ea0C796cCF039102B43341df24',
+    fromBlock: 12568633,
+  },
+  59140: {
+    // Linea Testnet
+    chainId: 59140,
+    providerUrl: `https://rpc.goerli.linea.build`,
+    tokenL2: '0xC007267DF5f0f7aEc5fb90CF03b56F051Bc6C89e',
+    fromBlock: 2053728,
   }
 }
 
@@ -54,9 +104,8 @@ const CONTRACT_L1 = new ethers.Contract(
   wallet
 )
 
-const ERC20_L2 = process.env.SEPOLIA_SCROLL_ERC20_ADDRESS
 const CONTRACT_L2 = new ethers.Contract(
-  ERC20_L2,
+  cfgL2.tokenL2,
   ABI_ERC20,
   PROVIDER_L2
 )
@@ -65,13 +114,12 @@ const CONTRACT_L2 = new ethers.Contract(
 let client
 
 
-async function getLogs(fromBlock, toBlock) {
-  // https://docs.alchemy.com/docs/deep-dive-into-eth_getlogs#what-are-event-signatures
+async function getLogs(fromBlock, toBlock, tokenAddress) {
   const logs = await PROVIDER_L2.getLogs(
     {
       "fromBlock": fromBlock,
       "toBlock": toBlock,
-      "address": ERC20_L2,
+      "address": tokenAddress,
       "topics": [
         "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
         // "0x0000000000000000000000000000000000000000000000000000000000000000",
@@ -89,8 +137,8 @@ async function getAddressBalance(address) {
 }
 
 
-async function storeBalances(logs) {
-  const storedBalances = await client.hGetAll(`chainId-${cfgL2.chainId}-balances`);
+async function storeBalances(chainId, logs) {
+  const storedBalances = await client.hGetAll(`chainId-${chainId}-balances`);
 
   for (let i=0; i<logs.length; i++) {
     const address = '0x' + logs[i].topics[2].slice(26)
@@ -101,7 +149,7 @@ async function storeBalances(logs) {
     }
 
     console.log("New Address&Balance: ", address, balance)
-    await client.hSet(`chainId-${cfgL2.chainId}-balances`, address, balance.toString())
+    await client.hSet(`chainId-${chainId}-balances`, address, balance.toString())
   }
 }
 
@@ -140,9 +188,9 @@ async function checkTree(tree, leafsData) {
     console.log("Leaf is ok")
 }
 
-async function sendNewRoot(root, blockNumber) {
-  console.log("Send chainId, root and blockNumber: ", cfgL2.chainId, root, blockNumber)
-  CONTRACT_L1.addStateRoot(cfgL2.chainId, ethers.toBeHex(root), blockNumber)
+async function sendNewRoot(chainId, root, blockNumber) {
+  console.log("Send chainId, root and blockNumber: ", chainId, root, blockNumber)
+  CONTRACT_L1.addStateRoot(chainId, ethers.toBeHex(root), blockNumber)
 }
 
 async function sleep() {
@@ -169,13 +217,13 @@ const main = async () => {
 
   let condition = true
 
-  let fromBlock = parseInt(process.env.FROM_BLOCK_NUMBER);
+  let fromBlock = cfgL2.fromBlock;
 
   while (condition) {
     const toBlock = (await PROVIDER_L2.getBlock()).number
     console.log(`BlockNumber: ${fromBlock}/${toBlock}`)
 
-    let logs = await getLogs(fromBlock, toBlock)
+    let logs = await getLogs(fromBlock, toBlock, cfgL2.tokenL2)
     if (fromBlock >= toBlock) {
       await sleep()
       continue
@@ -189,7 +237,7 @@ const main = async () => {
     // logs = logs.length > 5 ? logs.slice(0, 5) : logs
     // console.log("Logs number will be processed: ", logs.length)
 
-    await storeBalances(logs)
+    await storeBalances(cfgL2.chainId, logs)
     const storedBalances = await fetchBalances(cfgL2.chainId)
     const leafsData = await getTreeLeafs(storedBalances)
     const tree = await generateTree(process.env.TREE_HEIGHT, leafsData)
@@ -203,7 +251,7 @@ const main = async () => {
     // console.log(storedTree)
     // await checkTree(storedTree, leafsData)
 
-    await sendNewRoot(tree.root, fromBlock)
+    await sendNewRoot(cfgL2.chainId, tree.root, fromBlock)
 
     fromBlock = logs[logs.length-1].blockNumber + 1
 
